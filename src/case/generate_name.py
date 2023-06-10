ae6cfad790f3310867d68682663f393b4f58c6a4
yangshujie <yangshujie@fangcunyisheng.com>
2023-06-10 16:27:20 +0800

    fix 完成 prompt 的第一次案例

--- src/case/generate_name.py
+++ src/case/generate_name.py
@@ -1,7 +1,17 @@
+from langchain import PromptTemplate
 from langchain.llms import OpenAI
+
 
 def generateName():
     llm = OpenAI(temperature=0.9)
     text = "We would be a good company name for a company that make colorful socks."
     
     return llm(text)
+
+def genNameWithPrompt():
+    prompt = PromptTemplate(
+        input_variables=["product"],
+        template="what is a good name for a company that makes {product}?",
+    )
+    
+    return prompt.format(product="colorful socks")
--- tests/test_langchain.py
+++ tests/test_langchain.py
@@ -1,8 +1,17 @@
 from src.case.generate_name import generateName
 
 def test_generateName():
+    print("\ntest generate name: \n")
     result = generateName()
     print("\nresult: ", result)
 
     # 接下来进行断言
-    assert isinstance(result, str)    
+    assert isinstance(result, str)  
+    
+def test_generateNameWithPrompt():
+    print("\ntest generate name with prompt: \n")
+    result = generateName()
+    print("\nresult: ", result)
+
+    # 接下来进行断言
+    assert isinstance(result, str)     
