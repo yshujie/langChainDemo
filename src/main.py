2c5b8022401cea1e9001ccef4f26dec65edff245
yangshujie <yangshujie@fangcunyisheng.com>
2023-06-25 11:21:14 +0800

    test 运行 first_chat

--- src/main.py
+++ src/main.py
@@ -1,23 +1,7 @@
-from src.case import function_explainer
-
+from src.demo import first_chat
     
-def test_function_explainer():
-    fn_explainer = function_explainer.FunctionExplainerPromptTemplate(input_variables=["function_name"])    
-    
-    # Generate a prompt for the function "get_source_code"
-    prompt = fn_explainer.format(function_name=function_explainer.get_source_code)  
-    
-    print(prompt)
-    
-    
-from src.case import few_shot_prompt_template    
-def test_few_shot_prompt_template():
-    few_shot_prompt_template.using_example_selector()
-    
-    
-from src.case import few_shot_examples_for_chat_models
-def test_few_shot_examples_for_chat_models():
-    few_shot_examples_for_chat_models.alternationHumanAndAIMessage()    
+def test_first_chat():
+    first_chat.evaluateAI()    
     
 if __name__ == "__main__":
-    test_few_shot_examples_for_chat_models()
+    test_first_chat()
