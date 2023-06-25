e249fcbc9e9868d6b062c24e4485f20f5a7e5d00
yangshujie <yangshujie@fangcunyisheng.com>
2023-06-25 12:43:41 +0800

    test 测试 history_seatcher

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
-from src.demo import first_chat
+from src.demo import first_chat, history_searcher
     
 def test_first_chat():
     first_chat.evaluateAI()    
+
+def test_history_searcher():
+    history_searcher.searchHistoryOfToday()    
+    
     
 if __name__ == "__main__":
-    test_first_chat()
+    test_history_searcher()
