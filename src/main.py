3875457cb53e8120f4e4aa1aff3158695cde7168
yangshujie <yangshujie@fangcunyisheng.com>
2023-06-25 18:25:51 +0800

    feat 实现 document_summarizer demo

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
-from src.demo import first_chat, history_searcher
+from src.demo import first_chat, history_searcher, document_summarizer
     
 def test_first_chat():
     first_chat.evaluateAI()    
 
 def test_history_searcher():
     history_searcher.searchHistoryOfToday()    
+
+def test_document_summarizer():
+    summarizeContent = document_summarizer.summarize("./src/doc/RaAct.txt")
+    print(summarizeContent)
     
     
 if __name__ == "__main__":
-    test_history_searcher()
+    test_document_summarizer()
