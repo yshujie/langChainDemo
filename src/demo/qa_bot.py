c34a9591e13738628f4ecc3824844f2436a1de71
yangshujie <yangshujie@fangcunyisheng.com>
2023-06-26 09:23:41 +0800

    fix 本地知识问答机器人更新，使用 RetrievalQA 替换 VectorDBQA

--- src/demo/qa_bot.py
+++ src/demo/qa_bot.py
@@ -3,17 +3,22 @@ from langchain.vectorstores import Chroma
 from langchain.text_splitter import CharacterTextSplitter
 from langchain import OpenAI, VectorDBQA
 from langchain.document_loaders import DirectoryLoader
+from langchain.document_loaders import UnstructuredFileLoader
 from langchain.chains import RetrievalQA
+from langchain.vectorstores.base import VectorStoreRetriever
+
 
 def initTemporaryChroma() -> Chroma: 
     # 加载文件夹中所有的 txt 文件
-    loader = DirectoryLoader("data/qa", glob="*.txt")
+    dir_path = "./src/doc/qa/"
+    loader = DirectoryLoader(dir_path, glob='**/*.txt')
+                             
     # 将数据转成 document 对象
     documents = loader.load()
     
     # 初始化加载器
     text_splitter = CharacterTextSplitter(
-        chunk_size=100,
+        chunk_size=200,
         chunk_overlap=0
     )
     # 切割加载的 document
@@ -24,17 +29,14 @@ def initTemporaryChroma() -> Chroma:
     # 将 document 通过 openAI 的 embeddings 对象计算 embedding 向量信息，并临时存储 Chroma 向量数据库中
     return Chroma.from_documents(split_docs, embeddings)
 
-def question(question_str): 
+def question(query): 
     # 初始化向量数据库
-    chroma = initTemporaryChroma()
+    docsearch = initTemporaryChroma()
     
-    # 创建问答对象
-    qa = VectorDBQA().from_chain_type(
-        llm=OpenAI(),
-        chain_type="stuff",
-        vectorstore=chroma,
-        return_source_documents=True
-    )
+    # 创建问答对象    
+    retriever = VectorStoreRetriever(vectorstore=docsearch)          
+    qa = RetrievalQA.from_chain_type(llm=OpenAI(), retriever=retriever, chain_type="stuff", return_source_documents=True)
     
     # 提问
-    return qa({"question": question_str})
+    return qa({"query": query})
+
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from src.demo import first_chat, history_searcher, document_summarizer
+from src.demo import first_chat, history_searcher, document_summarizer, qa_bot
     
 def test_first_chat():
     first_chat.evaluateAI()    
@@ -9,7 +9,9 @@ def test_history_searcher():
 def test_document_summarizer():
     summarizeContent = document_summarizer.summarize("./src/doc/RaAct.txt")
     print(summarizeContent)
-    
+
+def test_qa_bot():
+    print(qa_bot.question("方寸的价值观是什么？简述一下"))
     
 if __name__ == "__main__":
-    test_document_summarizer()
+    test_qa_bot()
