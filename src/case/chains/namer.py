e9996456bf3651c7b005430da2190f09c35af12e
yangshujie <yangshujie@fangcunyisheng.com>
2023-06-28 10:48:34 +0800

    feat 新增 namer.py ，并实现给公司命名的功能

---
+++ src/case/chains/namer.py
@@ -0,0 +1,20 @@
+from langchain.llm import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
+
+def giveNameForCompany():
+    # 初始化 llm
+    llm = OpenAI(temperature=0.9)
+    
+    # 设置 Prompt
+    prompt = PromptTemplate(
+        input_variables=['product_name'],
+        template="What is a good name for a company that males {product_name}?"
+    )
+    
+    # 初始化链
+    chain = LLMChain(llm=llm, prompt=prompt)
+    
+    # 执行链
+    return chain.run("cars")    
