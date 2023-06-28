e7b0b9581d474a799c9b838ef301eb5f00dedac9
yangshujie <yangshujie@fangcunyisheng.com>
2023-06-28 11:34:29 +0800

    feat aanamer.py 实现异步逐次生成公司名称

--- src/case/chains/anamer.py
+++ src/case/chains/anamer.py
@@ -17,4 +17,18 @@ def generate_serially():
         resp = chain.run("cars")
         print(resp)
     
+async def async_generate(chain):
+    resp = await chain.run("cars")
+    print(resp)
+    
+async def generate_concurrently():
+    llm = OpenAI(temperature=0.9)
+    prompt = PromptTemplate(
+        input_variables=['product_name'],
+        template="What is a good name for a company that makes {product_name}?"
+    )
+    chain = LLMChain(llm=llm, prompt=prompt)
+    
+    tasks = [async_generate(chain) for _ in range(3)]
+    await asyncio.gather(*tasks)
     
