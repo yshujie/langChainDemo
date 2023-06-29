e9609bee9bba7faac8a0c0254b828c9bbd2a31bd
yangshujie <yangshujie@fangcunyisheng.com>
2023-06-29 09:36:29 +0800

    test 测试 router 链

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 from src.case.chains import (
     loading_from_langChainHub,
+    router
 )
 
 def testLoadFromLangChainHub():
     loading_from_langChainHub.calc()    
+
+def testRouter():
+    router.route()
     
 if __name__ == "__main__":
-    testLoadFromLangChainHub()
+    testRouter()
