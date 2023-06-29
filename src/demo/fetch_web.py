6566ebfbbd45529aa1f55c7399ce360de6eb6814
yangshujie <yangshujie@fangcunyisheng.com>
2023-06-29 11:45:15 +0800

    feat 实现使用 llm 抓去网页，并返回 json

---
+++ src/demo/fetch_web.py
@@ -0,0 +1,46 @@
+# in this demo, we will fetch a web page and output data as json format
+
+ from langchain.prompts import PromptTemplate
+ from langchain.llms import OpenAI
+ from langchain.chains import LLMRequestsChain, LLMChain
+ 
+ # init llm
+ llm = OpenAI(model_name="gpt-3.5-turbo", temperature=0)
+ 
+ # set prompt
+ template = """在 >>> 和 <<< 之间是网页返回的 HTML 内容。
+ 网页是新浪财经 A 股上市公司的简介。
+ 请抽取参数请求的信息。
+ 
+ >>> {requests_result} <<<
+ 
+ 请使用如下格式的 JSON 格式返回数据
+ {{
+        "company_name": "",
+        "company_english_name": "",
+        "issue_price": "",
+        "date_of_establishment": "",
+        "registered_capital": "",
+        "office_address": "",
+        "company_profile": "",
+ }}
+ Extracted:
+ """
+ 
+ prompt = PromptTemplate(
+     input_variables=["requests_result"],
+     template=template
+ )
+ 
+ # init chain
+ chain = LLMRequestsChain(
+     llm_chain=LLMChain(llm=llm, prompt=prompt)
+ )
+ 
+ def fetch_web(url):
+     inputs = {
+         "url": "https://vip.stock.finance.sina.com.cn/corp/go.php/vCI_CorpInfo/stockid/600519.phtml"
+     }
+     
+     response = chain(inputs)
+     print(response)
