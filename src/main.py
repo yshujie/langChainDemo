9c4fbac223d9a5e3b7375c8ebb15da491550313d
yangshujie <yangshujie@fangcunyisheng.com>
2023-06-29 08:52:02 +0800

    test 测试 使用 langChainHub 加载 chain

--- src/main.py
+++ src/main.py
@@ -1,22 +1,9 @@
-import asyncio
-import time 
-
 from src.case.chains import (
-    namer,
-    anamer
+    loading_from_langChainHub,
 )
 
-def testANamer():
-    s = time.perf_counter()
-    asyncio.run(anamer.generate_concurrently())
-    elapsed = time.perf_counter() - s
-    print("\033[1m]" + f"Concurrent execution in {elapsed:0.2f} seconds.\033[0m")
-    
-    s = time.perf_counter()
-    anamer.generate_serially()
-    elapsed = time.perf_counter() - s
-    print("\033[1m]" + f"Serial execution in {elapsed:0.2f} seconds.\033[0m")
-
+def testLoadFromLangChainHub():
+    loading_from_langChainHub.calc()    
     
 if __name__ == "__main__":
-    testANamer()
+    testLoadFromLangChainHub()
